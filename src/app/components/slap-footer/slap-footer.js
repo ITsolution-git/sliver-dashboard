73f239f7e85a8afb81dfe71c0be6b6daff800b89
VsevolodY <vsevolod@smallbizsilverlining.com>
2017-05-08 11:47:09 +0300

    fix layout

--- src/app/components/slap-footer/slap-footer.js
+++ src/app/components/slap-footer/slap-footer.js
@@ -3,17 +3,24 @@
 
     var slapFooter = {
         bindings: {},
-        controller: function ($scope, footerService,$timeout,$rootScope,$state) {
-            $timeout(function() {
+        controller: function ($scope, footerService, $timeout, $rootScope, $state) {
+            $timeout(function () {
                 $scope.state = footerService._state;
             });
 
-            $scope.next = function() {
+            $scope.next = function () {
                 $state.go($scope.state.next.sref);
+                $timeout(scrollTop);
             };
 
-            $scope.prev = function() {
+            $scope.prev = function () {
                 $state.go($scope.state.prev.sref);
+                $timeout(scrollTop);
+            };
+
+            function scrollTop() {
+                var body = $("body, html");
+                body.animate({scrollTop: 0}, 400);
             }
         },
         templateUrl: 'components/slap-footer/slap-footer.html'
