cb6c51cf8d7df9df79f49656aad1ff8b18b460cb
VsevolodY <vsevolod@smallbizsilverlining.com>
2017-05-12 19:09:28 +0300

    are yuor stuck controller send server

--- src/app/pages/mindset/are-your-stuck/are-your-stuck.controller.js
+++ src/app/pages/mindset/are-your-stuck/are-your-stuck.controller.js
@@ -6,19 +6,34 @@
         .controller('AreYourStuckController', AreYourStuckController);
 
     /* @ngInject */
-    function AreYourStuckController($scope,mindsetService,pageService) {
+    function AreYourStuckController($scope,mindsetService,pageService,stepService,$state) {
 
         angular.extend($scope, {
             showInfoBlock: false,
             showVideoBlock: false,
             showStaticTextBlock: false,
-            sliders: mindsetService.getSliders()
+            sliders: mindsetService.getSliders(),
+            forward: true
         });
+        $scope.sendData = sendData;
+
 
         pageService
             .reset()
             .setShowBC(false)
             .addCrumb({name: 'Dashboard', path: 'home'})
             .setPageTitle('Are You Stuck?');
+
+        function sendData() {
+            var urls = $state.current.name.split('.');
+            var data = angular.extend({}, {
+                sliders: $scope.sliders
+            });
+
+            return stepService.sendApiData(urls[urls.length - 1], data.sliders)
+                .then(function (response) {
+                    console.log(response);
+                });
+        }
     }
 }());
