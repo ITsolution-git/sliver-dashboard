fb5cf945efb03a86fa67b4cc752922250de4557b
a.karpenko <a.karpenko@smissltd.com>
2017-09-11 12:07:18 +0300

    Add Footer onto all pages

--- src/app/layouts/layout-blank.controller.js
+++ src/app/layouts/layout-blank.controller.js
@@ -7,5 +7,14 @@
 
     /* @ngInject */
     function LayoutBlankController($scope) {
+
+        $scope.$on('$stateChangeSuccess', 
+        function(event, toState, toParams, fromState, fromParams){ 
+            if(toState.name === "slapExcute.main"){
+                $scope.height = true;
+            }else {
+                $scope.height = false;
+            }
+         });
     }
 })();
