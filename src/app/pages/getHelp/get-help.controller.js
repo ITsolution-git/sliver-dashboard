aea90179151cc3d541ced327670cf142c94e50a1
a.karpenko <a.karpenko@smissltd.com>
2017-09-12 15:36:53 +0300

    Get Help added all Fields

--- src/app/pages/getHelp/get-help.controller.js
+++ src/app/pages/getHelp/get-help.controller.js
@@ -8,13 +8,18 @@
     /* @ngInject */
     function GetHelpController($scope, $auth, $state, toaster, pageService, userService, adminUserService) {
 
+        $scope.options = ['Strategy Question', 'Feeling Stuck', 'Question about SLAPschool', 
+        'Question about SLAPworld', 'Need to reschedule my SLAPexpert call', 'Need to reschedule my SLAPmanager call',
+        'Question about SLAPcenter', 'Tech Issue/Glitch', 'Other'];
+        $scope.optionPrefer = ['Call', 'Email'];
         $scope.sendMessage = function () {
             var sendObject = {
                 user_email: userService.user.email,
+                kind: $scope.option,
+                prefer: $scope.prefer,
                 message: $scope.message
             };
             userService.getHelp(sendObject).then(function (res) {
-                console.log(res);
                 toaster.pop({ type: 'success', body: "Email Sent successful!" });
             });
         }
