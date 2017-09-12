57c5dde11832687e63ed72531adc2f517d8fe090
a.karpenko <a.karpenko@smissltd.com>
2017-09-12 17:37:53 +0300

    fix bug in mainFooter

--- src/app/layouts/layout-blank.controller.js
+++ src/app/layouts/layout-blank.controller.js
@@ -8,13 +8,5 @@
     /* @ngInject */
     function LayoutBlankController($scope) {
 
-        $scope.$on('$stateChangeSuccess', 
-        function(event, toState, toParams, fromState, fromParams){ 
-            if(toState.name === "slapExcute.main"){
-                $scope.height = true;
-            }else {
-                $scope.height = false;
-            }
-         });
     }
 })();
