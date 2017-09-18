d765c3362f5b90d9ffaa1c1f9fd383ff8e557b1c
a.karpenko <a.karpenko@smissltd.com>
2017-09-18 14:19:45 +0300

    remove functionality that does not allow you to progress without updates

--- src/app/pages/actionPlan/world-around-your/world-around-your.controller.js
+++ src/app/pages/actionPlan/world-around-your/world-around-your.controller.js
@@ -76,13 +76,13 @@
         }
 
         function sendData(direction) {
-            var res = $scope.quaterChanged.every(function (quater) {
-                return quater;
-            });
-            if (!res && direction == 'forward' ) {
-                toaster.pop({ type: 'info', body: 'You must make adjustments to the information in all 4 Quarters before you can go to the next step' });
-                return false;
-            }
+            // var res = $scope.quaterChanged.every(function (quater) {
+            //     return quater;
+            // });
+            // if (!res && direction == 'forward' ) {
+            //     toaster.pop({ type: 'info', body: 'You must make adjustments to the information in all 4 Quarters before you can go to the next step' });
+            //     return false;
+            // }
             stepService.updateActiveModel($scope);
             stepService.setFinishActiveStep();
             
