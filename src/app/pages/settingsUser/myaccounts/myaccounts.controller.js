614a69ff8865e1604c817552377f9e642ce4f32b
a.karpenko <a.karpenko@smissltd.com>
2017-09-22 17:54:17 +0300

    spinner test

--- src/app/pages/settingsUser/myaccounts/myaccounts.controller.js
+++ src/app/pages/settingsUser/myaccounts/myaccounts.controller.js
@@ -33,7 +33,10 @@
                 $scope.user.avatarId = resp.data;
                 $rootScope.$emit('avatarUpdated', resp.data);
                 $('#avatar').on('load', function(){
+                    console.log('finish');
+                    console.log($scope.downloadFinished);
                     $scope.downloadFinished = true;
+                    console.log($scope.downloadFinished);
                 });
             },  function (response) {
                 $scope.downloadFinished = true;
