6eae0e04ac3946c61c6b4391ebefc2edf77043a4
a.karpenko <a.karpenko@smissltd.com>
2017-09-25 14:39:08 +0300

    add test video

--- src/app/components/video-block/video-block.js
+++ src/app/components/video-block/video-block.js
@@ -9,9 +9,11 @@
             video: '='
         }
     };
-    function videoController($scope, $sce) {
+    function videoController($scope, $sce, CONFIG) {
+        $scope.videoUrl = CONFIG.api +'/v1/video/SLAP_20.mp4';
         var vm = this;
         var defaulVideo = 'http://media.w3.org/2016/01/Emmy-Award.mp4';
+        vm.videoUrl = CONFIG.api + '/v1/video/SLAP_20.mp4';
         $scope.trustUrl = function(url){
             return url ? $sce.trustAsResourceUrl(url) : $sce.trustAsResourceUrl(defaulVideo);
         }
