f7c9036784d29a87dc22c098b674019e301b7f56
a.karpenko <a.karpenko@smissltd.com>
2017-09-27 20:14:54 +0300

    Replaced URL

--- src/app/components/video-block/video-block.js
+++ src/app/components/video-block/video-block.js
@@ -10,10 +10,12 @@
         }
     };
     function videoController($scope, $sce, CONFIG) {
-        $scope.videoUrl = CONFIG.api +'/v1/video/SLAP_20.mp4';
+        // $scope.videoUrl = CONFIG.api +'/v1/video/SLAP_20.mp4';
+        $scope.videoUrl = 'http://content.jwplatform.com/videos/UNz3tfmI-CZ1S4g0I.mp4';
         var vm = this;
         var defaulVideo = 'http://media.w3.org/2016/01/Emmy-Award.mp4';
-        vm.videoUrl = CONFIG.api + '/v1/video/SLAP_20.mp4';
+        // vm.videoUrl = CONFIG.api + '/v1/video/SLAP_20.mp4';
+        vm.videoUrl = 'http://content.jwplatform.com/videos/UNz3tfmI-CZ1S4g0I.mp4';
         $scope.trustUrl = function(url){
             return url ? $sce.trustAsResourceUrl(url) : $sce.trustAsResourceUrl(defaulVideo);
         }
