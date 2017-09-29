fd3a22443996c983715014652158684d205d3c27
a.karpenko <a.karpenko@smissltd.com>
2017-09-29 16:04:47 +0300

    fixed bug with error and fixed avatar in admin

--- src/app/admin/components/navbar/slap-navbar.js
+++ src/app/admin/components/navbar/slap-navbar.js
@@ -4,12 +4,31 @@
     var adminSlapNavbar = {
         bindings : {
         },
-        controller : function($auth,$state,userService,$scope, $window, permissionService) {
+        controller : function($auth,$state,userService,$scope, $window, permissionService, CONFIG, $rootScope) {
             var vm = this;
             vm.logout = function() {
                 $auth.logout();
                 $state.go('login');
             };
+            $rootScope.$on('SlapAccounUpdated', function (event, user) {
+                vm.user = user;
+                vm.avatarUrl = CONFIG.api + "/v1/user/avatar/" + user.avatarId;
+            });
+            $rootScope.$on('avatarUpdated', function (event, id) {
+                vm.user.avatarId = id;
+                vm.avatarUrl = CONFIG.api + "/v1/user/avatar/" + id;
+            });
+            // userService.getUser().then(function (user) {
+            //     vm.user = angular.copy(userService.user, {});
+            //     vm.avatarUrl = CONFIG.api+"/v1/user/avatar/"+user.avatarId;
+            // });
+            vm.user = userService.getStoredUser();
+            vm.avatarUrl = CONFIG.api + "/v1/user/avatar/" + vm.user.avatarId;
+            this.logout = function () {
+                $auth.logout();
+                $window.location.reload();
+                $state.go('login');
+            }
 
             vm.userService = userService;
             vm.isAuthenticated = false;
