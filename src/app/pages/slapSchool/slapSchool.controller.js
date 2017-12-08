f4c4119bf5ad7c1943327a3cf4ef6bcd8e2b393f
a.karpenko <a.karpenko@smissltd.com>
2017-12-08 13:10:11 +0200

    SLAPschool suggestions added

--- src/app/pages/slapSchool/slapSchool.controller.js
+++ src/app/pages/slapSchool/slapSchool.controller.js
@@ -6,13 +6,16 @@
         .controller('SlapSchoolController', SlapSchoolController);
 
     /* @ngInject */
-    function SlapSchoolController($scope, $state, data, toaster, pageService) {
+    function SlapSchoolController($scope, $state, data, toaster, pageService, userService) {
         pageService
         .setPageTitle('SLAPschool');
         
         var arr = [];
+        
         var webinars = data.webinars;
-
+        $scope.suggestionType = [];
+        $scope.types = [{id: 0, text: "Did a SLAPschool Event"}, {id: 1, text: "Added a Training or Tool"}, {id: 2, text: "Created an FAQ"}]
+        $scope.suggestionText = "";
         data.forEach(function(item){
             arr.push(item.name);
         });
@@ -51,6 +54,15 @@
                 toaster.pop({type: 'success', body: "This webinar is not live right now.  Please come back and join us when it starts!  It will be good!", timeout: 3000})
             }
         }  
+
+        $scope.sendSuggestion = function() {
+            userService.sendSuggestion({suggestionType: $scope.suggestionType, suggestionText: $scope.suggestionText}).then(function() {
+                $scope.suggestionType = [];
+                $scope.suggestionText = "";
+                toaster.pop({type: 'success', body: "Success!", timeout: 3000})
+            })
+
+        }
 
     }
 
